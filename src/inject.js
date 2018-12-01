f26a08db61b7725f51c92e8a6ab173ca586cff93
Tejas Shah <tejashah88@gmail.com>
2018-12-01 12:38:20 -0800

    Updated selectors to reflect new design

--- src/inject.js
+++ src/inject.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   if ($(projectListSelector).length) {
     var mainContestLink = window.location.href.split('/').slice(0, 5).join('/') + '/brief';
     $.get(mainContestLink).then(mainProjectPageData => {
-      var timelineObj = $('.section-thumbs', mainProjectPageData).last();
+      var timelineObj = $('[class*="side_panel__timelineContainer"]', mainProjectPageData).first();
       // get the project starting date
       var startDateString = $('p', timelineObj).eq(1).text().split('at')[0].trim();
       var startDate = new Date(startDateString);
@@ -12,24 +12,28 @@ $(document).ready(function() {
       // we don't want the first card, which is for the user to register for the challenge
       $(projectListSelector).each(function() {
         var projectObj = this;
-        var projectLink = 'https://www.hackster.io' + $('h4 > .project-link-with-ref', this).attr('href');
+        var isPrivate = $('h4 > [data-original-title="Private"]', this).length > 0;
+        // if it's private, accessing the project will throw a 401 error
+        if (!isPrivate) {
+          var projectLink = 'https://www.hackster.io' + $('h4 > .project-link-with-ref', this).attr('href');
 
-        $.get(projectLink).then(projectPageData => {
-          // retrieve the project published date
-          var possiblePubDate1 = $('[itemprop=datePublished]', projectPageData).text();
-          var possiblePubDate2 = $('[itemprop=dateCreated]', projectPageData).text();
-          var pubDateString = (possiblePubDate1 || possiblePubDate2).trim();
-          var pubDate = new Date(pubDateString);
+          $.get(projectLink).then(projectPageData => {
+            // retrieve the project published date
+            var possiblePubDate1 = $('[itemprop=datePublished]', projectPageData).text();
+            var possiblePubDate2 = $('[itemprop=dateCreated]', projectPageData).text();
+            var pubDateString = (possiblePubDate1 || possiblePubDate2).trim();
+            var pubDate = new Date(pubDateString);
 
-          var psaStyle = 'font-size:12px; white-space: pre-line';
-          if (pubDate.getTime() < startDate.getTime()) {
-            // exposed!!
-            $(projectObj).css('background', '#FD0');
-            var pubDatePSA = `<div style="${psaStyle}">Project published on ${pubDateString}</div>`;
-            var startDatePSA = `<div style="${psaStyle}">Contest started on ${startDateString}</div>`;
-            $(".spacer", projectObj).html(pubDatePSA + startDatePSA);
-          }
-        });
+            var psaStyle = 'font-size:12px; white-space: pre-line';
+            if (pubDate.getTime() < startDate.getTime()) {
+              // exposed!!
+              $(projectObj).css('background', '#FD0');
+              var pubDatePSA = `<div style="${psaStyle}">Project published on ${pubDateString}</div>`;
+              var startDatePSA = `<div style="${psaStyle}">Contest started on ${startDateString}</div>`;
+              $(".spacer", projectObj).html(pubDatePSA + startDatePSA);
+            }
+          });
+        }
       });
     });
   }
